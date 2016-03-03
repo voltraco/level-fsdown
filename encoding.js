50252a05c6ccd57eaab9b368e7a12a12d6bd5525
hij1nx <paolo@async.ly>
2016-03-03 23:08:38 +0100

    just be more explicit so we can cut down on the number of checks before encoding

--- encoding.js
+++ encoding.js
@@ -1,14 +1,14 @@
 const path = require('path')
-const dotRE = /\./g
+const cleanRE = /\W+/g
 
 exports.encode = function (source) {
   if (Array.isArray(source)) {
     var key = source.slice()
-    key.map(seg => seg.replace(dotRE, ''))
+    key.map(seg => seg.replace(cleanRE, ''))
     key[key.length - 1] += '.json'
     return path.join.apply(null, key)
   }
-  return source.replace(dotRE, '') + '.json'
+  return source.replace(cleanRE, '') + '.json'
 }
 
 exports.decode = function (source) {
